4a24f0d3809558e7437449fbdb5f8a48faa32a5b
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2025-06-11 00:22:37 -0500

    Se crea componente de card

---
+++ src/app/components/custom-card/custom-card.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CustomHeaderComponent } from '../custom-header/custom-header.component';
+import { CustomInputComponent } from '../custom-input/custom-input.component';
+import { CustomCheckboxComponent } from '../custom-checkbox/custom-checkbox.component';
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
+
+@Component({
+  selector: 'app-card-custom',
+  standalone: true,
+  imports: [
+    CustomHeaderComponent,
+    CustomInputComponent,
+    CustomCheckboxComponent,
+    CustomButtonComponent,
+  ],
+  templateUrl: './card-custom.component.html',
+})
+export class CardCustomComponent {
+  nombre = '';
+  descripcion = '';
+  estado = '';
+  implementacionObligatoria = false;
+
+  estados = [
+    { value: '1', text: 'Activo' },
+    { value: '2', text: 'Inactivo' },
+  ];
+
+  crear() {
+    alert(
+      `✅ Medición creada:\n\nNombre: ${this.nombre}\nDescripción: ${this.descripcion}\nEstado: ${this.estado}\nImplementación obligatoria: ${this.implementacionObligatoria ? 'Sí' : 'No'}`
+    );
+  }
+
+  cancelar() {
+    alert('❌ Creación cancelada');
+  }
+
+}
