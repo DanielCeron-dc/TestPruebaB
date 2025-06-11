985839288f20c4dbc9f7c9c2201f772d3ee78ec9
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2025-06-11 00:09:53 -0500

    Se crea componente de header

---
+++ src/app/components/custom-header/custom-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-custom-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './custom-header.component.html',
+  styleUrls: ['./custom-header.component.css'],
+})
+export class CustomHeaderComponent {
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() description: string = '';
+}
